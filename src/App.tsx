0087c5d32ed234be41724f0a981c8315d82f7705
tanyakanf <tanyakanf@gmail.com>
2024-08-25 17:15:24 +0200

    add vite react, ts, tailwind setup + routes

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,5 @@
+function LandingPage() {
+    return <h1 className="text-3xl font-bold">LandingPage</h1>
+}
+
+export default LandingPage
